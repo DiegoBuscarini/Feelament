0f5d8f85191304d0d0adbd55803844358c0ba345
diegoBusca <dgbuscarini@gmail.com>
2025-06-17 22:36:20 -0300

    Uso de Quokka

--- js/codigo.js
+++ js/codigo.js
@@ -1,13 +1,25 @@
-// let nombre
-// nombre = "Diego"
+let nombre
+nombre = "Diego"
+console.log (nombre)
+
+let suma = (2+5);
+console.log (suma);
 
 // alert ("Esta es una alerta de prueba")
 
-let nombre = prompt ("Decime tu nombre")
+// -----------------------------------------------------
+// let nombre = prompt ("Decime tu nombre")
 
-alert ("Hola " + nombre)
+// alert ("Hola " + nombre)
 
-let num1 = parseInt(prompt (nombre + " decime un número para sumar"))
-let num2 = parseInt(prompt (nombre + " decime otro número para sumar"))
-let resultado = num1+num2
-alert ("el resultado de la suma es: " + resultado)
+// let num1 = parseInt(prompt (nombre + " decime un número para sumar"))
+// let num2 = parseInt(prompt (nombre + " decime otro número para sumar"))
+// let resultado = num1+num2
+// alert ("el resultado de la suma es: " + resultado)
+// -----------------------------------------------------
+
+let iva = 21; 
+iva = 10.5; 
+console.log(iva);
+
+
